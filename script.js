8cd9566bc0ecfc99c811b424334ad9cb70c82ce0
Zianalysor <154446348+Zianalysor@users.noreply.github.com>
2024-11-18 19:39:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  document.getElementById('menu-toggle').addEventListener('click', () => {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('open');
+  });
+
